c9ba09d8db69f2b560686964cabcf69f2bf998bf
a-bulatov1 <abulatov1423@gmail.com>
2026-06-06 19:58:40 -0700

    Camera, platform, character

---
+++ scenes/level.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+var WIDTH = 10
+var wall_block: PackedScene = preload("res://scenes/wall_block.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for i in range(-5,5):
+		for j in range(-5, 5):
+			var cube = wall_block.instantiate()
+			cube.position.x = i
+			cube.position.z = j
+			add_child(cube)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
