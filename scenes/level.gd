29f5fec822bf3c4d55986dc2a3934598319ff5e0
a-bulatov1 <abulatov1423@gmail.com>
2026-06-16 20:08:34 -0700

    Lil bit of shaders

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,16 +1,11 @@
 extends Node3D
 
-var WIDTH = 10
-var wall_block: PackedScene = preload("res://scenes/wall_block.tscn")
+var FLOORWIDTH = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for i in range(-5,6):
-		for j in range(-5, 6):
-			var cube = wall_block.instantiate()
-			cube.position.x = i
-			cube.position.z = j
-			add_child(cube)
+	$Floor.scale.x = FLOORWIDTH
+	$Floor.scale.z = FLOORWIDTH
 	
 
 
